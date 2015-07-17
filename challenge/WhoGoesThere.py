e1896c7ec5907176abe06e48362f4ada04d7ea1d
Kai Wang <charles.wangkai@gmail.com>
2015-07-17 10:57:49 +0800

    Who goes there?

---
+++ challenge/WhoGoesThere.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Q: http://www.hacker.org/challenge/chal.php?id=17
+# A: http://www.hacker.org/challenge/chal.php?answer=yoblaog&id=17&go=Submit
+
+def main():
+    print(''.join(reversed('goalboy')))
+
+if __name__ == '__main__':
+    main()
