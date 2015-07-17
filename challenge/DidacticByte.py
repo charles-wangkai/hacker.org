006bd62ca20ae4cd27e0f82466bcae62b9ca2dfa
Kai Wang <charles.wangkai@gmail.com>
2015-07-17 10:51:58 +0800

    Didactic Byte

---
+++ challenge/DidacticByte.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Q: http://www.hacker.org/challenge/chal.php?id=105
+# A: http://www.hacker.org/challenge/chal.php?answer=e9&id=105&go=Submit
+
+def main():
+    print(hex(233)[2:])
+    
+if __name__ == '__main__':
+    main()
