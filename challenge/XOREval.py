0bbcebd8521397095c4154c7ed3455af1b5a6b46
Kai Wang <charles.wangkai@gmail.com>
2015-07-17 11:28:09 +0800

    XOR Eval

---
+++ challenge/XOREval.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Q: http://www.hacker.org/challenge/chal.php?id=2
+# A: http://www.hacker.org/challenge/chal.php?answer=61&id=2&go=Submit
+
+def main():
+    print(17 ^ 39 ^ 11)
+
+if __name__ == '__main__':
+    main()
