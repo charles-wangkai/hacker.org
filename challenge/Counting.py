057bfd8fc6d46e16d352f243d061645ff21a0cb0
Kai Wang <charles.wangkai@gmail.com>
2015-07-19 10:45:48 +0800

    Counting

---
+++ challenge/Counting.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Q: http://www.hacker.org/challenge/chal.php?id=137
+# A: http://www.hacker.org/challenge/chal.php?answer=592&id=137&go=Submit
+
+def main():
+    text = 'eehxhqpmawoewdffplqrturxdjlsaylymgxjsthjpacyuxnpuvqlezhosbnmmjzeeahjllnacofwyxxrelwgadsmolyynahrfvqkqonkgjsazwczwbayptsnsuvyomalyisyroxbivlqvtaltvjwtqbsbnscqmdcwxxdkvwctbynbvokdcovbebokjlmekezpcnoxvzzpaqhusdhgbhtqzeuoegylofircjlxdypcvekkllxjxlynidhgngtpblebyoazqvoccnhauwcsviqlbzsmyrproffqapjtizlrdasradufbjwhkllykgtrqivlrsrwswzdwjuktqgzkyslucqxgtseafofbhvhltparprjunrsivyhmelkkodvukwkoiwmhunbjmhtrvowapwuvogjqcaxwepbxoynhygxsqmbcavzvfydrptedyvbzrqficmrobquqvtcjoclyedsafxlhlmyxeyeumiswjjzdxxdqccyqvobspwhsmazmabshscmlquplbmhvvuiuasmjjajwyoyezgvxhpfteblvcuxhuosoekqtiobyvbdytyycyesmzkvbcupnbp'
+    print(len(text))
+
+if __name__ == '__main__':
+    main()
