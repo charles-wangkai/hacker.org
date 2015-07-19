2ba2848fdc0e4ac2effcf398bf874f7871efdd72
Kai Wang <charles.wangkai@gmail.com>
2015-07-19 17:09:22 +0800

    Type Fast [Make it reusable]

--- challenge/TypeFast.py
+++ challenge/TypeFast.py
@@ -9,13 +9,16 @@ from challenge import hacker_org_util
 
 PROBLEM_ID = '76'
 
-def main():
-    source = urllib.request.urlopen(hacker_org_util.build_challenge_url(PROBLEM_ID)).read().decode()
+def solve(problem_id, prompt):
+    source = urllib.request.urlopen(hacker_org_util.build_challenge_url(problem_id)).read().decode()
     
-    answer = re.search(r'Please type the sentence you see below quickly.*<b>(.*)</b>', source, flags=re.DOTALL).group(1).strip()
+    answer = re.search(r'{prompt}.*<b>(.*)</b>'.format(prompt=prompt), source, flags=re.DOTALL).group(1).strip()
     print(answer)
     
-    print(urllib.request.urlopen(hacker_org_util.build_challenge_url(PROBLEM_ID, answer)).read().decode())
+    print(urllib.request.urlopen(hacker_org_util.build_challenge_url(problem_id, answer)).read().decode())
+
+def main():
+    solve(PROBLEM_ID, 'Please type the sentence you see below quickly')
 
 if __name__ == '__main__':
     main()
