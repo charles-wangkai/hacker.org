2d7c55b7a678cc720c208edde89e27f867ddb7f6
Kai Wang <charles.wangkai@gmail.com>
2015-07-19 17:09:47 +0800

    Type Faster

---
+++ challenge/TypeFaster.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Q: http://www.hacker.org/challenge/chal.php?id=77
+# A: (Not fixed answer, so run this script)
+
+from challenge import TypeFast
+
+PROBLEM_ID = '77'
+
+def main():
+    TypeFast.solve(PROBLEM_ID, 'Please type the paragraph you see below quickly')
+
+if __name__ == '__main__':
+    main()
