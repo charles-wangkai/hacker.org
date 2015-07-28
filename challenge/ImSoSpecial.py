8dee9b9777e1f3d4ab8f801dacf22de48efc3aea
Kai Wang <charles.wangkai@gmail.com>
2015-07-28 13:56:51 +0800

    I'm So Special [Remove unnecessary web page opening]

--- challenge/ImSoSpecial.py
+++ challenge/ImSoSpecial.py
@@ -4,16 +4,10 @@
 # A: (My current IP address. Not fixed answer, so run this script)
 
 import urllib.request
-import webbrowser
-from challenge import hacker_org_util
-
-PROBLEM_ID = '198'
 
 def main():
     answer = urllib.request.urlopen('http://bot.whatismyipaddress.com/').read().decode()
     print(answer)
-    
-    webbrowser.open(hacker_org_util.build_challenge_url(PROBLEM_ID, answer))
 
 if __name__ == '__main__':
     main()
