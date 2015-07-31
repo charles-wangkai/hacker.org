cb89e1cee0e88c7c6b39912ddb102708528a5b31
Kai Wang <charles.wangkai@gmail.com>
2015-07-31 23:46:42 +0800

    Valuation [Add missing answer's URL]

--- challenge/Valuation.py
+++ challenge/Valuation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Q: http://www.hacker.org/challenge/chal.php?id=153
-# A: 
+# A: http://www.hacker.org/challenge/chal.php?answer=1492&id=153&go=Submit
 
 def main():
     text = '93752xxx746x27x1754xx90x93xxxxx238x44x75xx08750912738x8461x8759383xx328x4x4935903x6x5550360535004x0xx945958961296x267x8842xxx5x6xx61x4x48482x80xxx83316843x7x4x83x9521731xxx25x51xx457x6x5x9698222x771237745034x5133592x27xx8x87xx35221x36x0x50x23x7x63x998418xx'
