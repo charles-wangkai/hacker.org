bed36cfce15185b5b82667ac5ba11d081fe35e70
Kai Wang <charles.wangkai@gmail.com>
2015-08-06 20:14:57 +0800

    Add add_username_password_to_url()

--- challenge/hacker_org_util.py
+++ challenge/hacker_org_util.py
@@ -29,14 +29,24 @@ def configure_cookies():
     opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(http.cookiejar.CookieJar()))
     urllib.request.install_opener(opener)
 
+def add_username_password_to_url(url_str):
+    parse_result = urllib.parse.urlparse(url_str)
+    
+    query = urllib.parse.parse_qs(parse_result.query)
+    query.update({'name': username, 'password': password})
+    parameters = urllib.parse.urlencode(query, doseq=True)
+    
+    updated_parts = list(parse_result)
+    updated_parts[4] = parameters
+    return urllib.parse.urlunparse(updated_parts)
+    
 def build_challenge_url(problem_id, answer=None):
-    query = {'id': problem_id, 'name': username, 'password': password}
+    query = {'id': problem_id}
     if answer:
-        query['answer'] = answer
-        query['go'] = 'Submit'
+        query.update({'answer': answer, 'go': 'Submit'})
     
     parameters = urllib.parse.urlencode(query)
-    return '{base_challenge_url}?{parameters}'.format(base_challenge_url=BASE_CHALLENGE_URL, parameters=parameters)
+    return add_username_password_to_url('{base_challenge_url}?{parameters}'.format(base_challenge_url=BASE_CHALLENGE_URL, parameters=parameters))
 
 
 init_username_password()
