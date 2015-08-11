e0d3d581f69dfc3fccc038d3333bafdb2687efe1
Kai Wang <charles.wangkai@gmail.com>
2015-08-12 02:37:14 +0800

    Some Randoms

---
+++ challenge/SomeRandoms/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Q: http://www.hacker.org/challenge/chal.php?id=72
+# A: http://www.hacker.org/challenge/chal.php?answer=0.119355+0.254839&id=72&go=Submit
+
+gnatmake testran.adb
+./testran
