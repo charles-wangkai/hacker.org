2c8afcf7bad78339a5d0a2167b54d55a52d27913
Kai Wang <charles.wangkai@gmail.com>
2015-08-12 05:05:21 +0800

    Hello World

---
+++ challenge/HelloWorld/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Q: http://www.hacker.org/challenge/chal.php?id=63
+# A: http://www.hacker.org/challenge/chal.php?answer=89*P+99*45*%2BP+22*3*3*3*0^PP+66*1%2B3*P+67*2%2BP+48*P+99*6%2BP+66*1%2B3*P+89*67*%2BP+22*3*3*3*P+89*47*%2BP&id=63&go=Submit
+
+../../hackvm.py program.txt
+cat program.txt
