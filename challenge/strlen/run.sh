d3442a2dca9c4aca9f3de1969de2e00cc5e158de
Kai Wang <charles.wangkai@gmail.com>
2017-03-24 18:43:11 +0800

    strlen

---
+++ challenge/strlen/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Q: http://www.hacker.org/challenge/chal.php?id=66
+# A: http://www.hacker.org/challenge/chal.php?answer=00%5E%3C8%3F1%2B09-4-gp%21&id=66&go=Submit
+
+../../hackvm.py --init memory.txt program.txt
+cat program.txt
