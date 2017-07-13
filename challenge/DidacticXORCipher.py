02cd62a4107998b7d791692add89ddb81805ebd1
Kai Wang <charles.wangkai@gmail.com>
2017-07-13 17:32:19 +0800

    Didactic XOR Cipher [Make it reusable]

--- challenge/DidacticXORCipher.py
+++ challenge/DidacticXORCipher.py
@@ -3,8 +3,11 @@
 # Q: http://www.hacker.org/challenge/chal.php?id=157
 # A: http://www.hacker.org/challenge/chal.php?answer=random+seed&id=157&go=Submit
 
+def decrypt_xor_cipher(cipher, key):
+    return ''.join(map(lambda byte: chr(byte ^ key), bytes.fromhex(cipher)))
+
 def main():
-    print(''.join(map(lambda byte: chr(byte ^ 79), bytes.fromhex('3d2e212b20226f3c2a2a2b'))))
+    print(decrypt_xor_cipher('3d2e212b20226f3c2a2a2b', 79))
 
 if __name__ == '__main__':
     main()
