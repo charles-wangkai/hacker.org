3c898b602c588ac0bee559456afa4e77360201c9
Kai Wang <charles.wangkai@gmail.com>
2017-07-15 09:44:25 +0800

    Fast Mod

---
+++ challenge/FastMod/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Q: http://www.hacker.org/challenge/chal.php?id=122
+# A: http://www.hacker.org/challenge/chal.php?answer=0%3C0%3C1%3C%2F1%3C*-p&id=122&go=Submit
+
+../../hackvm.py --init memory.txt program.txt
+cat program.txt
