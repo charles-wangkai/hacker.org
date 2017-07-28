5abbaed9234f1676f8d2b416de6ddfa8b064dc18
Kai Wang <charles.wangkai@gmail.com>
2017-07-29 00:49:33 +0800

    It's a Small World

---
+++ challenge/ItsASmallWorld/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Q: http://www.hacker.org/challenge/chal.php?id=67
+# A: http://www.hacker.org/challenge/chal.php?answer=0089*-05-396036*-5%5E1-3308-4045*2%2B-5%5E0%5E34*%2B63304-4%5E47*%2B0%5E27*%3F+357**%2BP045*-g&id=67&go=Submit
+
+../../hackvm.py program.txt
+cat program.txt
