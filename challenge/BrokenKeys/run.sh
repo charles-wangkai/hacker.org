376d0df2ed5a5e9ae8511031043b4518c945daa0
Kai Wang <charles.wangkai@gmail.com>
2017-08-02 20:39:03 +0800

    Broken Keys [Add reference]

--- challenge/BrokenKeys/run.sh
+++ challenge/BrokenKeys/run.sh
@@ -3,5 +3,7 @@
 # Q: http://www.hacker.org/challenge/chal.php?id=212
 # A: http://www.hacker.org/challenge/chal.php?answer=0%3C0%3C1%3C-00%3C1%3C-2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F2%2F-*-p&id=212&go=Submit
 
+# http://www.programmerinterview.com/index.php/general-miscellaneous/find-max-without-comparison/
+
 ../../hackvm.py --init memory.txt program.txt
 cat program.txt
